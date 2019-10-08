a291799632b901a42c322df4ddbab64311612ae4
valeria campilongo <cvaleriac@hotmail.it>
2019-10-08 16:30:53 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,7 +19,7 @@ class LoginForm extends React.Component {
      if (!this.state.username || !this.state.password) return
 
      this.props.handleLogin(this.state)
-  }  
+  }
 
   render() {
     return (
